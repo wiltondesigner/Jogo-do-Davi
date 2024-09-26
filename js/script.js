d686d38c1755073c46a107186bba79119e073958
wiltondesigner <wiltondesigner@gmail.com>
2024-09-26 16:58:00 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,51 +1,46 @@
 function recarregarPagina() {
     window.location.reload();
   }
-
-document.addEventListener('DOMContentLoaded', () => {
-    const jumpSound = new Audio('./sounds/jump.mp3');
-    const gameOverSound = new Audio('./sounds/game-over.mp3');
-    
-    // Defina o volume se necessário
-    jumpSound.volume = 0.5;
-    gameOverSound.volume = 0.5;
-    
-    const davi = document.querySelector('.davi');
-    const coco = document.querySelector('.coco');
-
-    const jump = () => {
-        davi.classList.add('jump');
-        jumpSound.play();  // Toca o som de pulo
-        
-        setTimeout(() => {
-            davi.classList.remove('jump');
-        }, 500);
-    }
-
-    const loop = setInterval(() => {
-
-        const cocoPosition = coco.offsetLeft;
-        const daviPosition = +window.getComputedStyle(davi).bottom.replace('px','');
-
-        if (cocoPosition <= 90 && cocoPosition > 0 && daviPosition < 90) {
-            coco.style.animation = 'none';
-            coco.style.left = `${cocoPosition}px`;
-
-            davi.style.animation = 'none';
-            davi.style.bottom = `${daviPosition}px`;
-
-            davi.src = './image/game-over.png';
-
-            gameOverSound.play();  // Toca o som de game over
-            clearInterval(loop);
-        }
-
-    }, 10);
-
-     
-      // Adiciona suporte para teclado (desktop)
-    document.addEventListener('keydown', jump);
-
-    // Adiciona suporte para toque (celulares/tablets)
-    document.addEventListener('touchstart', jump);
-});
+  
+  const davi = document.querySelector('.davi');
+  const coco = document.querySelector('.coco');
+  
+  // Carregar os sons
+  const jumpSound = new Audio('./sounds/jump.mp3');  // Som do pulo
+  const gameOverSound = new Audio('./sounds/game-over.mp3');  // Som de game over
+  
+  const jump = () => {
+      davi.classList.add('jump');
+      jumpSound.play();  // Tocar som de pulo
+  
+      setTimeout(() => {
+          davi.classList.remove('jump');
+      }, 500);
+  }
+  
+  // Detecta toque na tela para pular (para celulares)
+  document.addEventListener('touchstart', jump);
+  
+  // Detecta pressionamento de tecla para pular (para desktops)
+  document.addEventListener('keydown', jump);
+  
+  const loop = setInterval(() => {
+  
+      const cocoPosition = coco.offsetLeft;
+      const daviPosition = +window.getComputedStyle(davi).bottom.replace('px','');
+  
+      if (cocoPosition <= 52 && cocoPosition > 0 && daviPosition < 90) {
+          coco.style.animation = 'none';
+          coco.style.left = `${cocoPosition}px`;
+  
+          davi.style.animation = 'none';
+          davi.style.bottom = `${daviPosition}px`;
+  
+          davi.src = './image/game-over.png';
+          
+          gameOverSound.play();  // Tocar som de game over
+          clearInterval(loop);
+      }
+  
+  }, 10);
+  
